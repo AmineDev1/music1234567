6faf4225435c7eb1c2432c85232b524b4fd37e12
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-19 11:41:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-var prefix = "1";
+var prefix = "2";
 var adminprefix = 's'
 /////////////////////////
 ////////////////////////
@@ -345,7 +345,7 @@ client.on('message', message => {
 
 
 
-const developers = ["472951521798127626","506832717137182733"]
+const developers = ["445406863048048642"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
